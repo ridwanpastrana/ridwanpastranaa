aa2b18f4f04ebc3064ed84a252a3463fb62c8d25
Ridwan P <ridwanpastrana@yahoo.com>
2017-10-29 16:00:03 +0700

    commit

---
+++ Untitled-1.js
@@ -0,0 +1,34 @@
+Number 
+const x = 10;
+
+//string
+String
+const apos_string ='Hello';
+const quot_string = "Hello";
+const tick_string = 'Hello';
+
+
+//boolean
+const b = true
+
+//array
+
+const r = [0, 1, 2, 3];
+
+//object
+
+const p = {
+    1:1,
+    Nama: 'Ridwan',
+    Kelas: 'Web',
+};
+
+console.log(p.Nama)
+console.log(p.Kelas)
+
+//Function
+
+
+
+
+
